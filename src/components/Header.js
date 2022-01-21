0a361cf7ed24e31e69f803dd9536d1640bd0b4fa
kingvyas <kumarvyasss@gmail.com>
2022-01-21 22:13:04 +0530

    app

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="header">
+    <nav>
+      <h1 className="right">Meme Generator</h1>
+      <h4 className="left">The Vyas Foundation</h4>
+    </nav>
+    </header>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+import memes from "./memesData";
+import {useState} from 'react'
+
+export default function Main() {
+  //We should pass callback function inside useState setter, if original and previous State is required to perform a function
+  const [imgUrl, setImgUrl] = useState(false);
+  function handleClick(){
+    setImgUrl(memes[Math.floor(Math.random() * memes.length)].url)
+  }
+  return (
+    <main>
+      <div className="wrapper">
+        <div className="input-box">
+          <input
+            type="text"
+            className="top textInput"
+            placeholder="Enter top text"
+          />
+          <input
+            type="text"
+            className="bottom textInput"
+            placeholder="Enter bottom text"
+          />
+        </div>
+        <div className="button-box">
+          <button onClick={handleClick}>Generate New Random Meme</button>
+        </div>
+      </div>
+      {imgUrl && <div className="image-box">
+        <img src={imgUrl} alt="" />
+      </div>}
+    </main>
+  );
+}
