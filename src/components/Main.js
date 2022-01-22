067d18286cfa8f1af97c3dd2df87cf0ae95ecc95
kingvyas <kumarvyasss@gmail.com>
2022-01-22 12:34:45 +0530

    change-yes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,52 @@
 import memes from "./memesData";
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function Main() {
-  // We should pass callback function inside useState setter, if original and previous State is required to perform a function
   const [imgUrl, setImgUrl] = useState(false);
-  function handleClick(){
-    setImgUrl(memes[Math.floor(Math.random() * memes.length)].url)
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  function handleClick() {
+    setImgUrl(memes[Math.floor(Math.random() * memes.length)].url);
   }
+
+  function topChange(e) {
+    setTopText(e.target.value);
+  }
+
+  function bottomChange(e) {
+    setBottomText(e.target.value);
+  }
+
   return (
     <main>
       <div className="wrapper">
         <div className="input-box">
           <input
             type="text"
-            className="top textInput"
+            className="top-input textInput"
             placeholder="Enter top text"
+            value={topText}
+            onChange={topChange}
           />
           <input
             type="text"
-            className="bottom textInput"
+            className="bottom-input textInput"
             placeholder="Enter bottom text"
+            value={bottomText}
+            onChange={bottomChange}
           />
         </div>
         <div className="button-box">
           <button onClick={handleClick}>Generate New Random Meme</button>
         </div>
       </div>
-      {imgUrl && <div className="image-box">
-        <img src={imgUrl} alt="" />
-      </div>}
+      {imgUrl && (
+        <div className="image-box">
+          {topText && <h3 className="top">{topText}</h3>}
+          {bottomText && <h3 className="bottom">{bottomText}</h3>}
+          <img src={imgUrl} alt="" />
+        </div>
+      )}
     </main>
   );
 }
